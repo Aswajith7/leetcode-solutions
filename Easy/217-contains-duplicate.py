a887a35bc539712d00520dcee5b689075c66e82b
Aswajith7 <165001655+Aswajith7@users.noreply.github.com>
2025-12-14 21:39:40 +0530

    Create 217-contains-duplicate.py
    Example 1:
    
    Input: nums = [1,2,3,1]
    
    Output: true
    
    Explanation:
    
    The element 1 occurs at the indices 0 and 3.
    
    Example 2:
    
    Input: nums = [1,2,3,4]
    
    Output: false
    
    Explanation:
    
    All elements are distinct.

---
+++ Easy/217-contains-duplicate.py
@@ -0,0 +1,11 @@
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        check = set(nums)
+        if len(check)== len(nums):
+            return False
+        else :
+            return True
+        
